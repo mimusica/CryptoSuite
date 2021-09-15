54605b7d71fb32b6f122a17776303a400d7625da
Christophe Langenberg <christophe@langenberg.be>
2021-09-15 21:35:30 +0200

    [TESTING][DEV TOOLS] initialized the unittest file for config.logging

---
+++ tests/unit_tests/test_config_logging.py
@@ -0,0 +1,24 @@
+"""
+Unittesting the logging configuration of our CryptoSuite
+"""
+import logging
+import unittest
+import sys
+import config.logger as logger
+from datetime import datetime
+
+
+class LoggerTestCase(unittest.TestCase):
+    FORMATTER = logging.Formatter(
+        "%(asctime)s [%(name)s][%(levelname)s] %(message)s"
+    )
+    TIME = str(datetime.today())[:-7]
+    FORMATTED_TIME = TIME.replace("-", "_").replace(" ", "__").replace(":", "_")
+
+    LOG_DIR = "./log"
+    LOG_FILE = f"{LOG_DIR}/{str(__file__).split('.', 1)[0]}_{FORMATTED_TIME}.log"
+
+    def test_get_console_handler(self):
+        test_console_handler = logging.StreamHandler(sys.stdout)
+        test_console_handler.setFormatter(self.FORMATTER)
+        self.assertEqual(logger.get_console_handler(), test_console_handler)
