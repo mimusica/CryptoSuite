d405f41b60f1fa4159512d08dee6cc2de03eb4fb
Christophe Langenberg <christophe@langenberg.be>
2021-09-16 18:03:04 +0200

    [ENHANCEMENT] added the blockchain explorer placeholders

---
+++ cryptosuite/dapps/explorer/binance.py
@@ -0,0 +1,10 @@
+"""
+The Binance Smart Chain explorer
+"""
+
+import web3
+from generic import Explorer
+
+
+class Binance(Explorer):
+    pass
---
+++ cryptosuite/dapps/explorer/generic.py
@@ -0,0 +1,7 @@
+"""
+A generic base class for blockchain explorers
+"""
+
+
+class Explorer:
+    pass
