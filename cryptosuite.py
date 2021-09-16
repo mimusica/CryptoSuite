126eef173ce4ab8ad9734ae1f2407aff83ccb888
Christophe Langenberg <christophe@langenberg.be>
2021-09-16 18:04:03 +0200

    [ENHANCEMENT] added placeholders for our cryptosuite

---
+++ cryptosuite.py
@@ -0,0 +1,16 @@
+"""
+Welcome to the CryptoSuite!
+
+Author: christophe@langenberg.be
+
+In our Suite you'll find a nice ensemble of tools that assist with the
+interaction of various segments of the Crypto universe.
+"""
+
+
+class CryptoSuite:
+    pass
+
+
+if __name__=='main':
+    CryptoSuite()
