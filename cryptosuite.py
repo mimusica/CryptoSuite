9e4cb1d8605f87bb82f81b6a32c644d7cbfafd1f
Christophe Langenberg <christophe@langenberg.be>
2021-09-23 00:29:46 +0200

    [CI/CD][ENHANCEMENT] added automatic version tagging to the github workflow

--- cryptosuite.py
+++ cryptosuite.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Welcome to the CryptoSuite!
 
@@ -6,11 +7,16 @@ Author: christophe@langenberg.be
 In our Suite you'll find a nice ensemble of tools that assist with the
 interaction of various segments of the Crypto universe.
 """
+from argparse import ArgumentParser
 
+from cryptosuite import __version__
 
-class CryptoSuite:
-    pass
+parser = ArgumentParser()
+parser.add_argument('-v', '--version',
+                    help="Version info",
+                    action='store_true'
+                    )
+args = parser.parse_args()
 
-
-if __name__=='main':
-    CryptoSuite()
+if args.version:
+    print(str(__version__).strip("(").strip(")").replace(",", ".").replace(" ", ""))
