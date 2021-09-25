0cb0285a5de870e1cf8f86afed6b3a4fb0a12218
Christophe Langenberg <christophe@langenberg.be>
2021-09-25 23:26:54 +0200

    [TDD][REFACTOR] github issue #33 refactor the logger

--- tests/unit_tests/test_config_logging.py
+++ tests/unit_tests/test_config_logging.py
@@ -6,7 +6,7 @@ import sys
 import unittest
 from datetime import datetime
 
-from cryptosuite.config.logger import Logger
+# from cryptosuite.config.logger import Logger
 
 
 class LoggerTestCase(unittest.TestCase):
